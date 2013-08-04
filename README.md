3c9ceab9630fe847c078b7f39fbe16799bf3af48
Christopher Brown <io@henrian.com>
2013-08-04 17:35:59 -0500

    Major refactor and rename to support new flickr-with-uploads API and CLI execution of API calls

--- README.md
+++ README.md
@@ -1,45 +1,150 @@
-# Flickr Backup
+# Flickr Sync
 
-Flickr offers unlimited storage for Pro accounts, and they allow you to have private photos, so they are an excellent photo backup service. They have a great API, and lots of apps. But I don't trust those apps, so I made my own backup script.
+Flickr offers unlimited storage for Pro accounts, and one terabyte to free accounts (that's 100,000 10MB pictures). They allow you to have private photos, so they are an excellent photo backup service. They have [a great API](http://www.flickr.com/services/api/), and lots of apps. But I don't trust those apps (and many of the backup ones cost money), so I made my own backup script.
 
-## Requirements:
+## Installation
 
-* [homebrew](https://github.com/mxcl/homebrew)
+1. Get `Node.js` and `npm` if you don't already have them.
+    - To see if you have node, run `node --version` and if it returns something like `v0.10.15`, you do.
+    - [Node.js install instructions](http://chbrown.github.io/#nodejs--npm)
+2. Install the `flickr-sync` package:
+    - `npm -g install flick-sync`
+3. Get OAuth credentials for using the Flickr API:
+    - Go to [www.flickr.com/services/apps/](https://www.flickr.com/services/apps/)
+    - Click "Get another key" on the right.
+    - Click "Apply for a non-commercial key"
+    - Fill in some details and click the checkboxes, and you should get to a page with "Key:" and "Secret:" fields, fancy gold lettering on a black velvet background towards the top.
+        * The "Key" field is your `consumer_key`.
+        * The "Secret" field is your `consumer_secret`.
+    - Here's the complicated part. At this point you need to get an OAuth token and secret that links your "app" with your "user", since the "app" you just "applied for" and created can't really do anything without users.
+        * You can follow the process outlined in the [Flickr OAuth documentation](http://www.flickr.com/services/api/auth.oauth.html).
+        * Or you can try out my [AutoAuth](https://github.com/chbrown/autoauth) package for help on getting these credentials together, perhaps a little easier than the official way, if you happen to have phantomjs already installed.
+    - Eventually you'll need to end up with a file of credentials at `~/.flickr` or thereabouts, which looks like this:
 
-You should have this already.
+```json
+{
+  "consumer_key": "i0LOwemEyB7SHoQgzGfvxPKjhlIbuDYs",
+  "consumer_secret": "FnM2RhOwXjB5tVrl",
+  "oauth_token": "FYSWxIJTGwvDHc98P-0n2tVdLUkmQsCBOX3",
+  "oauth_token_secret": "H8RcVM96oLtA0Gpd"
+}
+```
 
-* [node](https://github.com/joyent/node)
+(Those values are made up -- you'll need to get your own.)
 
-Simple: `brew install node`
+## Usage
 
-* [flickr-with-uploads](https://github.com/chbrown/flickr-with-uploads)
-* [optimist](https://github.com/substack/node-optimist)
-* [glob](https://github.com/isaacs/node-glob)
+### `test` command
 
-Just `npm install` to install those from the package.json file.
+To test whether everything installed okay, run `flickr test`, which should print something like the following, only with your user id and username:
 
-## Setup:
+```json
+{
+  "user": {
+    "id": "33947520@N00",
+    "username": {
+      "_content": "audiere"
+    }
+  },
+  "stat": "ok"
+}
+```
 
-Make a new app on Flickr, and create a file called `.env` in the same directory as `backup.js`.
-The `.env` file should look like this (after all the auth has been set up):
+### `api` command
 
-    FLICKR_API_KEY=i0LOwemEyB7SHoQgzGfvxPKjhlIbuDYs
-    FLICKR_API_SECRET=FnM2RhOwXjB5tVrl
-    FLICKR_OA_TOKEN=FYSWxIJTGwvDHc98P-0n2tVdLUkmQsCBOX3
-    FLICKR_OA_TOKEN_SECRET=H8RcVM96oLtA0Gpd
+Once you have the credentials in place, you can issue individual commands to the Flickr API.
 
-See [AutoAuth](https://github.com/chbrown/autoauth) for help on getting these credentials together. It's a bit of a pain, I know.
+**Upload a single file:**
 
-* `FLICKR_API_KEY=` *the "App Key"*
-* `FLICKR_API_SECRET=` *the "App Secret"*
-* `FLICKR_OA_TOKEN=` *access_token=??? output*
-* `FLICKR_OA_TOKEN_SECRET=` *access_token_secret=??? output*
+    flickr api -m upload title=CrashReport photo=~/Desktop/crash-report.jpg
 
-## Instructions
+Note that this interface does not change the default public settings, unlike `flickr sync`, below! It only fills in the necessary OAuth credentials and signs the url.
 
-Basically, it just reads from a folder, which should contain a list of other folders (which in turn correspond to photosets) containing images.
+**Get the [Flickr pandas](http://www.flickr.com/services/api/flickr.panda.getList.html):**
 
-For example, here is a smidgen of my Pictures directory:
+    flickr api -m flickr.panda.getList
+
+Result:
+
+```json
+{
+  "pandas": {
+    "panda": [
+      {
+        "_content": "ling ling"
+      },
+      {
+        "_content": "hsing hsing"
+      },
+      {
+        "_content": "wang wang"
+      }
+    ]
+  },
+  "stat": "ok"
+}
+```
+
+The CLI requires the `--method` (`-m`) flag to be set, and interprets any other positional arguments as parameters:
+
+    flickr api -m flickr.photos.getInfo photo_id=7500858540
+
+Result:
+
+```json
+{
+  "photo": {
+    "id": "7500858540",
+    "dateuploaded": "1341407391",
+    "license": "4",
+    ...
+    "owner": {
+      "username": "audiere",
+      ...
+    },
+    ...
+    "media": "photo"
+  },
+  "stat": "ok"
+}
+```
+
+You can also pipe in a file of parameters.
+
+Let's say you accidentally created a lot of duplicates of one photoset, and you want to delete them all. The Flickr web UI is pretty slow and you can only delete one photoset at a time, so this could take a while. Let's say that you want to delete all your photosets that contain the string "Too many lemurs":
+
+```bash
+flickr api -m flickr.photosets.getList --select photosets.photoset | \
+  json -C id title._content | \
+  grep 'Too many lemurs' | \
+  awk '{printf "photoset_id=%s\n",$1}' | \
+  flickr api -m flickr.photosets.delete
+```
+
+* The `--select` flag pulls out a specific value or set of values from the response. If you select an array, it will output one json entry per line.
+    - In this case, `--select photosets.photoset` selects the list of photoset object entries in the `photoset` field, which is a property of the root-level `photosets` dictionary.
+* `json -C ...` filters out everything but a few of the fields (the photoset name and id) in and tab-separates them.
+    - [`npm install -g json`](https://github.com/zpoley/json-command) to instal
+* `awk` pulls out just the photoset id, and puts the appropriate argument name in front of the value.
+* Piping into `flickr api` will hit the API at `flickr.photosets.delete` once for each line of input.
+
+### `sync` command
+
+The star of the show, the sync helper will read a directory of directories of pictures files, and upload each photo as a member of a photoset corresponding to its parent album. Flickr [only supports](http://www.flickr.com/help/photos/#150488231) jpegs, gifs, pngs, and tiffs, so I match on this case-insensitive glob: `{gif,png,jpg,jpeg,tif,tiff}`. If the destination photoset does not already exist, it will be created.
+
+All images are uploaded as completely private:
+
+```json
+{
+  ...
+  is_public: 0,
+  is_friend: 0,
+  is_family: 0,
+  hidden: 2
+}
+```
+
+For example, here is a snippet of my `~/Pictures` directory:
 
     /Users/chbrown/Pictures/
       - 20120710 Sarahs/
@@ -56,8 +161,14 @@ For example, here is a smidgen of my Pictures directory:
         - VB7O3453.JPG
         - VB7O3454.JPG
 
-Each picture is about 10 MB, so for this structure and size, I would run:
+Each of my pictures happens to be about 10 MB, and I'm on my home wifi, so I would run:
 
-    node backup.js --dir=/Users/chbrown/Pictures --workers=5
+    flickr sync --directory ~/Pictures --workers 5
 
 For smaller pictures, or if you have a whole lot of bandwidth, you can add more workers.
+
+    flickr sync --directory ~/Pictures --workers 20
+
+## License
+
+Copyright © 2011–2013 Christopher Brown. [MIT Licensed](LICENSE).
--- models.js
+++ models.js
@@ -1,173 +1,246 @@
-var flickr_client = require('./flickr_client');
+'use strict'; /*jslint es5: true, node: true, indent: 2 */ /* globals setImmediate */
 var fs = require('fs');
+var glob = require('glob');
+var path = require('path');
 var async = require('async');
-var logerr = function(err) { if (err) console.log('ERR', err); };
+var logger = require('winston');
+var _ = require('underscore');
+var stream = require('stream');
 
+var FlickrPhoto = function(attributes) {
+  _.extend(this, attributes);
+};
+FlickrPhoto.fromJSON = function(obj) {
+  return new FlickrPhoto({
+    id: obj.id,
+    title: obj.title,
+    is_public: obj.ispublic,
+    is_friend: obj.isfriend,
+    is_family: obj.isfamily,
+  });
+};
 
-function FlickrDatabase(backup_cover_photo) {
-  this.backup_cover_photo = backup_cover_photo;
-
-  // photosets is {photoset_title: <FlickrPhotoset> object, ...}
+var FlickrDatabase = exports.FlickrDatabase = function(api) {
+  /**
+    api: initialized Flickr client request function
+  */
+  this.api = api;
+  // this.cover_photo = Object<FlickrPhoto> with 'id' property to use for future uploads
+  this.cover_photo = null;
+  // this.photosets = { photoset_title: Object<FlickrPhotoset>, ...}
+  // however, those flickr photosets may not be initialized
+  this.photosets = null;
+};
+FlickrDatabase.prototype.initialize = function(callback) {
+  // callback signature: function(err)
+  // before this runs, FlickrDatabase.photosets == null
+  // after this runs, FlickrDatabase.photosets will be at least {}
+  var self = this;
   this.photosets = {};
-}
-FlickrDatabase.prototype.init = function(photoset_titles, callback) {
-  var self = this;
-  this.syncPhotosets(function() {
-    // preload albums
-    async.each(photoset_titles, function(photoset_title, next) {
-      self.getPhotoset(photoset_title, function(photoset) {
-        console.log('Syncing photoset: ' + photoset.title);
-        photoset.sync(next);
-      });
-    }, callback);
+  async.auto({
+    coverPhoto: function(callback, context) {
+      // get the backup-cover-photo (Flickr requires photosets to have cover photos)
+      self.api({
+        method: 'flickr.photos.search',
+        user_id: 'me',
+        tags: 'api'
+      }, callback);
+    },
+    photosetList: function(callback, context) {
+      self.api({
+        method: 'flickr.photosets.getList',
+        per_page: 500
+      }, callback);
+    },
+  }, function(err, context) {
+    var first_search_result = context.coverPhoto.photos.photo[0];
+    logger.debug('Calling FlickrPhoto.fromJSON: ' + JSON.stringify(first_search_result, null, '  '));
+    self.cover_photo = FlickrPhoto.fromJSON(first_search_result);
+    logger.debug('Using cover_photo for all backups: %s', self.cover_photo.id);
+
+    context.photosetList.photosets.photoset.forEach(function(photoset_json) {
+      var photoset = FlickrPhotoset.fromJSON(self.api, photoset_json);
+      // self.photosets is a map: { photoset_title -> FlickrPhotoset object, ... }
+      self.photosets[photoset.title] = photoset;
+    });
+    logger.debug('Found %d photosets.', Object.keys(self.photosets).length);
+
+    callback(err);
   });
 };
 
-FlickrDatabase.prototype.addPhotoset = function(raw_photoset) {
-  var title = raw_photoset.title._content,
-    photoset = this.photosets[title] = new FlickrPhotoset(title, raw_photoset);
-  return photoset;
-};
-FlickrDatabase.prototype.syncPhotosets = function(callback) {
-  // callback signature: ()
-  var self = this;
-  flickr_client.api('flickr.photosets.getList', {per_page: 500}, function(err, response) {
-    logerr(err);
-    response.photosets.photoset.forEach(function(photoset) {
-      self.addPhotoset(photoset);
-    });
-    callback();
-  });
-};
 FlickrDatabase.prototype.getPhotoset = function(photoset_title, callback) {
-  // callback signature: (<FlickrPhotoset> object)
+  // callback signature: (err, Object<FlickrPhotoset>)
   var self = this;
   var photoset = this.photosets[photoset_title];
   if (photoset) {
-    setImmediate(function() { callback(photoset) });
-  }
-  else {
-    var data = {title: photoset_title,
-      description: 'flickr-store',
-      primary_photo_id: this.backup_cover_photo.id};
-    flickr_client.api('flickr.photosets.create', data, function(err, response) {
-      logerr(err);
-      flickr_client.api('flickr.photosets.getInfo', {photoset_id: response.photoset.id}, function(err, response) {
-        logerr(err);
-        var photoset = self.addPhotoset(response.photoset);
-        callback(photoset);
-      });
-    });
-  }
-};
-
-
-
-function FlickrPhotoset(title, raw) {
-  // photos is {photo_title: photo_object}
-  this.title = title;
-  this.raw = raw;
-
-  this.photos = {};
-  this.total_pages = 1000;
-  this.synced = false;
-}
-FlickrPhotoset.prototype.addPhoto = function(raw_photo) {
-  var title = raw_photo.title;
-  var photo = this.photos[title] = new FlickrPhoto(title, raw_photo);
-  return photo;
-};
-FlickrPhotoset.prototype.sync = function(callback) {
-  // callback signature: ()
-  var self = this;
-  if (!this.synced) {
-    this.syncPhotos(function() {
-      self.synced = true;
-      callback();
-    }, 1);
-  }
-  else {
-    setImmediate(callback);
-  }
-};
-FlickrPhotoset.prototype.syncPhotos = function(callback, page) {
-  // callback signature: ()
-  // if (page === undefined) page = 1;
-  // page through all the photos, assume the photoset exists
-  var self = this;
-  var data = {photoset_id: this.raw.id, page: page};
-  flickr_client.api('flickr.photosets.getPhotos', data, function(err, response) {
-    logerr(err);
-    self.total_pages = response.photoset.pages;
-    // response.photoset.photo is an array of photo_objects
-    response.photoset.photo.forEach(function(raw_photo) {
-      self.addPhoto(raw_photo);
-    });
-
-    if (page < self.total_pages) {
-      // console.log('Recursing on flickr.photosets.getPhotos', data, page + 1, '<', self.total_pages);
-      self.syncPhotos(callback, page + 1);
+    if (photoset.photos) {
+      callback(null, photoset);
     }
     else {
-      callback();
+      photoset.syncPhotos(function(err) {
+        callback(err, photoset);
+      });
     }
-  });
-};
-
-
-
-function FlickrPhoto(title, raw) {
-  this.title = title;
-  this.raw = raw;
-}
-
-
-
-
-function LocalPhoto(title, photoset_title, fullpath) {
-  this.title = title;
-  this.photoset_title = photoset_title;
-  this.fullpath = fullpath;
-}
-LocalPhoto.prototype.existsInPhotoset = function(photoset_title, database, callback) {
-  // callback signature: (<bool> exists)
-  var self = this;
-  var photoset = database.photosets[photoset_title];
-  if (photoset) {
-    photoset.sync(function() {
-      var photo = photoset.photos[self.title];
-      return callback(photo !== undefined);
-    });
   }
   else {
-    setImmediate(function() { callback(false); });
+    photoset = new FlickrPhotoset(self.api, {
+      title: photoset_title,
+      description: 'flickr-sync',
+      primary_photo_id: self.cover_photo.id,
+    });
+
+    // must create the photoset if it does not exist
+    photoset.create(function(err) {
+      // photoset.photos for a new photoset will be null
+      if (err) {
+        callback(err, photoset);
+      }
+      else {
+        photoset.syncPhotos(function(err) {
+          callback(err, photoset);
+        });
+      }
+    });
   }
 };
-LocalPhoto.prototype.upload = function(photoset_title, fullpath, database, callback) {
-  // callback signature: (error)
+
+function FlickrPhotoset(api, attributes) {
+  this.api = api;
+  _.extend(this, attributes);
+  // this.photos = {photo_title: Object<FlickrPhoto>, ...}
+}
+FlickrPhotoset.prototype.create = function(callback) {
+  /** simply persist the photoset described by this' properties to Flickr
+    callback signature: (err)
+  */
+  logger.debug('Creating photoset, "%s"', this.title);
   var self = this;
-  database.getPhotoset(photoset_title, function(photoset) {
-    var params = {
-      title: self.title, description: 'flickr-store', tags: 'flickr-store',
-      is_public: 0, is_friend: 0, is_family: 0, hidden: 2,
-      photo: fs.createReadStream(fullpath, {flags: 'r'})
-    };
-    flickr_client.api('upload', params, function(err, response) {
-      if (err) return callback(err);
-      var photo_id = response.photoid;
-      flickr_client.api('flickr.photosets.addPhoto', {photoset_id: photoset.raw.id, photo_id: photo_id}, function(err, response) {
-        if (err) return callback(err);
-        flickr_client.api('flickr.photos.getInfo', {photo_id: photo_id}, function(err, response) {
-          photoset.addPhoto(response.photo);
-          callback(err);
+  this.api({
+    method: 'flickr.photosets.create',
+    title: this.title,
+    description: this.description,
+    primary_photo_id: this.primary_photo_id,
+  }, function(err, response) {
+    if (!err) {
+      self.id = response.photoset.id;
+    }
+    callback(err);
+  });
+  // async.waterfall([], callback);
+  // function(response, callback) {
+  //   self.api({
+  //     method: 'flickr.photosets.getInfo',
+  //     photoset_id: response.id,
+  //   }, callback);
+  // }
+};
+FlickrPhotoset.prototype.syncPhotos = function(callback) {
+  // callback signature: (err)
+  // page through all available photos, assuming the photoset exists
+  // caching retrieved photos in this.photos
+  this.photos = {};
+  var self = this;
+  (function next(page) {
+    self.api({method: 'flickr.photosets.getPhotos', photoset_id: self.id, page: page}, function(err, response) {
+      if (err) {
+        logger.error('Error in syncPhotos: flickr.photosets.getPhotos.');
+        callback(err);
+      }
+      else {
+        // response.photoset.photo is an array of photo_objects
+        response.photoset.photo.forEach(function(photo_json) {
+          var photo = FlickrPhoto.fromJSON(photo_json);
+          self.photos[photo.title] = photo;
         });
-      });
+
+        if (page < response.photoset.pages) {
+          next(page + 1);
+        }
+        else {
+          callback();
+        }
+      }
     });
+  }(1));
+};
+FlickrPhotoset.prototype.getPhoto = function(photo_title) {
+  // might return null
+  return this.photos[photo_title];
+};
+FlickrPhotoset.prototype.upload = function(local_photo, callback) {
+  // callback signature: function(err)
+  var self = this;
+  async.auto({
+    upload: function(callback, context) {
+      self.api({
+        method: 'upload',
+        title: local_photo.name,
+        description: 'flickr-sync',
+        tags: 'flickr-sync',
+        is_public: 0,
+        is_friend: 0,
+        is_family: 0,
+        hidden: 2,
+        photo: fs.createReadStream(local_photo.filepath), // {flags: 'r'} by default
+      }, callback);
+    },
+    addPhoto: ['upload', function(callback, context) {
+      self.api({
+        method: 'flickr.photosets.addPhoto',
+        photoset_id: self.id,
+        photo_id: context.upload.photoid._content,
+      }, callback);
+    }],
+    getInfo: ['upload', 'addPhoto', function(callback, context) {
+      self.api({
+        method: 'flickr.photos.getInfo',
+        photo_id: context.upload.photoid._content,
+      }, callback);
+    }],
+  }, function(err, context) {
+    var photo = FlickrPhoto.fromJSON(context.getInfo.photo);
+    self.photos[photo.title] = photo;
+    callback(err);
   });
 };
-LocalPhoto.prototype.toString = function() {
-  return this.title;
+FlickrPhotoset.fromJSON = function(api, obj) {
+  logger.debug('Creating FlickrPhotoset from JSON:', JSON.stringify(obj));
+  return new FlickrPhotoset(api, {
+    id: obj.id,
+    title: obj.title._content,
+    description: obj.description._content,
+    primary_photo_id: obj.primary,
+  });
 };
 
-exports.FlickrDatabase = FlickrDatabase;
-exports.LocalPhoto = LocalPhoto;
+var LocalPhoto = exports.LocalPhoto = function(album, name, filepath) {
+  /** create a representation of a photo that exists on the local filesystem.
+    album: The name of the photoset to upload this photo to (usually the parent directory's name)
+    name: The title to use for this photo (usually the filename)
+    filepath: The fully-specified location of the photo on the local filesystem
+  */
+  this.album = album;
+  this.name = name;
+  this.filepath = filepath;
+};
+LocalPhoto.stream = function(root) {
+  /** returns a Readable stream of LocalPhoto objects */
+  var output = stream.Readable({objectMode: true});
+  // supported types listed here: http://www.flickr.com/help/photos/#150488231
+  // trust Glob not to give me repeats
+  var glob_stream = new glob.Glob('*/*.{gif,png,jpg,jpeg,tif,tiff}', {cwd: root, nocase: true})
+  .on('match', function(match) {
+    var album = path.dirname(match);
+    var photo = path.basename(match);
+    var local_photo = new LocalPhoto(album, photo, path.join(root, match));
+    output.emit('data', local_photo);
+  })
+  .on('error', function(err) {
+    output.emit('error', err);
+  })
+  .on('end', function() {
+    output.push(null);
+  });
+  return output;
+};
